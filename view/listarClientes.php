4efea22e367891042ebff90d7ee3401a5149ecad
gabriel <gabrielnama2@gmail.com>
2023-07-07 15:58:26 -0300

    CRUD de clientes finalizado

--- view/listarClientes.php
+++ view/listarClientes.php
@@ -2,7 +2,7 @@
 require_once '../model/Cliente.inc.php';
 require_once './includes/cabecalho.inc.php';
 ?>
-<h1 class="text-center">Listar Clientes</h1>
+<h1 class="text-center">Listar Clientes</h1><br>
 <?php
 if (isset($_SESSION['clientes'])) {
     $clientes = $_SESSION['clientes'];
@@ -25,8 +25,10 @@ if (isset($_SESSION['clientes'])) {
                     <td><?= $cliente->getNome() ?></td>
                     <td><?= $cliente->getEmail() ?></td>
                     <td>
+                        <!-- Editar -->
                         <a href="editarCliente.php?id=<?= $cliente->getId() ?>" class="btn btn-primary"><i class="ti ti-edit"></i></a>
-                        <a href="../controllers/controllerCliente.php?opcao=6&id=<?= $cliente->getId() ?>" class="btn btn-danger"><i class="ti ti-trash"></i></a>
+                        <!-- Excluir -->
+                        <a href="#" class="btn btn-danger" onclick="excluirCliente(<?= $cliente->getId() ?>)"><i class="ti ti-trash"></i></a>
                     </td>
                 </tr>
             <?php
@@ -34,6 +36,13 @@ if (isset($_SESSION['clientes'])) {
             ?>
         </tbody>
     </table>
+    <script>
+        function excluirCliente(clienteId) {
+            if (confirm("Tem certeza de que deseja excluir o cliente?")) {
+                window.location.href = "../controllers/controllerCliente.php?opcao=6&id=" + clienteId;
+            }
+        }
+    </script>
 <?php
 }
 
