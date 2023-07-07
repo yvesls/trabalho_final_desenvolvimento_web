b905f0855fc48bd046f31228c9dd75688f5564df
gabriel <gabrielnama2@gmail.com>
2023-07-07 16:49:49 -0300

    Layout modificado

--- view/includes/cabecalho.inc.php
+++ view/includes/cabecalho.inc.php
@@ -14,7 +14,7 @@ $cliente = $_SESSION["clienteLogado"];
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
+  <title>Domésticos 007</title>
   <link rel="shortcut icon" type="image/png" href="./assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="./assets/css/styles.min.css" />
 </head>
